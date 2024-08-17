c673b89e9883438e963bb7991953896bccfdd22b
Mubashir Ali <21sw071@students.muet.edu.pk>
2024-08-17 13:46:57 -0700

    Working on Express Project 2

--- Task_13_Backend_Projects/04-store-api/starter/app.js
+++ Task_13_Backend_Projects/04-store-api/starter/app.js
@@ -1,1 +1,26 @@
-console.log('04 Store API')
+import express from 'express';
+import dotenv from 'dotenv';
+import {notFound} from './middleware/not-found.js'
+import {errorHandlerMiddleware} from './middleware/error-handler.js'
+
+// Server initialization
+const app = express();
+dotenv.config();
+
+// Middlewares in Use
+app.use(express.json())
+
+// Api Routes
+app.get('/' , (req , res) => {
+    res.send({
+        msg : "This working fine"
+    })
+})
+
+// Error Handler Middlewares
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+// Port on which server is running
+app.listen(3000, console.log("The server is running on Port 3000"));
+
--- Task_13_Backend_Projects/04-store-api/starter/db/connect.js
+++ Task_13_Backend_Projects/04-store-api/starter/db/connect.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const connectDB = (url) => {
+export const connectDB = (url) => {
   return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -9,4 +9,4 @@ const connectDB = (url) => {
   })
 }
 
-export default connectDB;
+
