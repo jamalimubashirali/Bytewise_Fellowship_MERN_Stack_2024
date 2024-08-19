01df0fb3d4f93c6f24fe9a72824fd4faae86c0b9
Mubashir Ali <21sw071@students.muet.edu.pk>
2024-08-19 08:56:38 -0700

    Added Basic Filters

--- Task_13_Backend_Projects/04-store-api/starter/controllers/products.js
+++ Task_13_Backend_Projects/04-store-api/starter/controllers/products.js
@@ -2,11 +2,21 @@ import Product from '../models/product.js';
 
 export const getAllProducts = async (req , res) => {
     try {
-        const data = await Product.find({ 
-            company : "liddy"
-        });
-        console.log(data.length);
+        const { featured , company , name} = req.query;
+        const queryObject = {};
+        if(featured) {
+            queryObject.featured = featured === 'true' ? true : false ;
+        }
+        if(company) {
+            queryObject.company = company ;
+        }
+        if(name) {
+            queryObject.name = { $regex : name , $options : 'i'} ;
+        }
+        console.log(queryObject);
+        const data = await Product.find(queryObject);
         res.status(200).json(data);
+        console.log(data.length);
     } catch (error) {
         console.log(error);
     }
@@ -15,9 +25,11 @@ export const getAllProducts = async (req , res) => {
 
 export const getAllProductsStatic = async (req , res) => {
     try {
-        res.status(200).json({
-            msg : "This is all static Products"
-        }); 
+        const data = await Product.find({ 
+            company : "liddy"
+        });
+        console.log(data.length);
+        res.status(200).json(data);
     } catch (error) {
         console.log(error);
     }
